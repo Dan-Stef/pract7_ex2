1152ef63b55b8e0b8e5ddad467336faa5eeb1c0e
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-06-21 14:42:00 +0300

    Добавьте файлы проекта.

---
+++ pract7_ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract7_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение: ");
+            string text = (Console.ReadLine());
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length != 1)
+                    Console.Write(words[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
